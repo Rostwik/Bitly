f514ae3f896047d165ab4e7b914449a6947ea0ca
Rostislav <rostwik@gmail.com>
2021-10-17 15:21:11 +0300

    Made the function names clear

--- main.py
+++ main.py
@@ -30,16 +30,16 @@ def main():
 
     if not is_bitlink(user_url, bitly_token):
         print(f'Для {user_url} еще не был создан bitlink')
-        print('Ваш новый bitlink:', shorted_link(user_url, bitly_token))
+        print('Ваш новый bitlink:', get_shorted_link(user_url, bitly_token))
         return
 
     print(
         'Такая короткая ссылка нашлась, кол-во кликов по ней:',
-        count_clicks(user_url, bitly_token)
+        get_count_clicks(user_url, bitly_token)
     )
 
 
-def parsed_link(url_for_parse):
+def get_parsed_link(url_for_parse):
     parse_url = urlparse(url_for_parse)
     url_without_sheme = f'{parse_url.netloc}{parse_url.path}'
 
@@ -48,12 +48,12 @@ def parsed_link(url_for_parse):
 
 def is_bitlink(url, token):
     bitly_header = {'Authorization': f'Bearer {token}'}
-    bit_url = f'https://api-ssl.bitly.com/v4/bitlinks/{parsed_link(url)}'
+    bit_url = f'https://api-ssl.bitly.com/v4/bitlinks/{get_parsed_link(url)}'
     response = requests.get(bit_url, headers=bitly_header)
     return response.ok
 
 
-def shorted_link(url, token):
+def get_shorted_link(url, token):
     bitly_header = {'Authorization': f'Bearer {token}'}
     bit_url = 'https://api-ssl.bitly.com/v4/bitlinks'
     bitly_body = {'long_url': url, 'title': 'Mybit'}
@@ -64,10 +64,10 @@ def shorted_link(url, token):
     return response.json()['id']
 
 
-def count_clicks(url, token):
+def get_count_clicks(url, token):
     bitly_params = {'unit': 'day', 'units': '-1'}
     bit_url = 'https://api-ssl.bitly.com/v4/bitlinks/' \
-              f'{parsed_link(url)}/clicks/summary'
+              f'{get_parsed_link(url)}/clicks/summary'
 
     bitly_header = {'Authorization': f'Bearer {token}'}
     response = requests.get(bit_url, headers=bitly_header, params=bitly_params)
